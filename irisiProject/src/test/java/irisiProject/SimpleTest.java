d16137279012158f500d5cd9f77ab7e34d9688fc
Said <oubellasd@gmail.com>
2019-11-25 21:16:20 +0100

    JUnit

---
+++ irisiProject/src/test/java/irisiProject/SimpleTest.java
@@ -0,0 +1,17 @@
+package irisiProject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimpleTest {
+
+	@Test
+	void test() {
+    	int a=5;
+    	int b=2;
+    	int x=5+2;
+    	assertEquals(7, x);
+	}
+
+}
